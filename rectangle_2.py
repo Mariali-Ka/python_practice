307da59d49f56f40b6f1dc42c869344b738f6760
Mariali-Ka <ma.karkashenko@gmail.com>
2022-11-23 18:36:54 +0700

    Module 16.8.2

---
+++ rectangle_2.py
@@ -0,0 +1,30 @@
+from rectangle import Rectangle, Square, Circle
+
+
+
+rect_1 = Rectangle(3, 4)
+rect_2 = Rectangle(12, 5)
+
+print(rect_1.get_area())
+print(rect_2.get_area())
+
+square_1 = Square(5)
+square_2 = Square(10)
+
+print(square_1.get_area_square(),
+      square_2.get_area_square())
+
+circ_1 = Circle(3)
+circ_2 = Circle(5)
+
+print(circ_1.get_area_circle(),
+      circ_2.get_area_circle())
+
+figures = [rect_1, rect_2, square_1, square_2, circ_1, circ_2]
+for figure in figures:
+      if isinstance(figure, Square):
+            print("True-it is a square:", figure.get_area_square())
+      elif isinstance(figure, Circle):
+            print("True- it is a circle:", figure.get_area_circle())
+      else:
+            print(figure.get_area())
